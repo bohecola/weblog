b402ef6ef940a1d25289177e353f16e0e6bb2ff0
bohecola <bohecola@outlook.com>
2022-02-19 19:28:01 +0800

    baseURL

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api/weblog';
+const dev = 'http://localhost:3000/api/weblog'
+const prod = 'http://canday.site:3000/api/weblog'
+
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? dev : prod;
 axios.defaults.timeout = 5000;
 
 axios.interceptors.request.use(
