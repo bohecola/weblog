aafeb44475d634192810c2256c2ecb49fa89ed71
kongli <bohecola@outlook.com>
2022-02-21 22:58:43 +0800

    文章没有标签时，详情不展示tag图标

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -10,7 +10,7 @@
           {{ article.category?.name }}
         </span>
 
-        <el-icon><price-tag /></el-icon>
+        <el-icon v-if="article.tags.length > 0"><price-tag /></el-icon>
         <template v-for="(tag, index) in article.tags" :key="tag.name">
           <span class="article-tags" @click="$router.push({ name: 'TagList', query: { tagId: tag._id, name: tag.name } })">
             {{ tag.name }}
